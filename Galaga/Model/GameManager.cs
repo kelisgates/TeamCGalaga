cff67bb03a532c2eb7861dd3e22c47ca935dba11
kg00281 <kg00281@CSVLAB-VM30>
2024-10-21 16:22:18 -0400

    Add project files.

---
+++ Galaga/Model/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+
+namespace Galaga.Model
+{
+    /// <summary>
+    /// Manages the Galaga game play.
+    /// </summary>
+    public class GameManager
+    {
+        #region Data members
+
+        private const double PlayerOffsetFromBottom = 30;
+        private readonly Canvas canvas;
+        private readonly double canvasHeight;
+        private readonly double canvasWidth;
+
+        private Player player;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="GameManager"/> class.
+        /// </summary>
+        public GameManager(Canvas canvas)
+        {
+            this.canvas = canvas ?? throw new ArgumentNullException(nameof(canvas));
+
+            this.canvas = canvas;
+            this.canvasHeight = canvas.Height;
+            this.canvasWidth = canvas.Width;
+
+            this.initializeGame();
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void initializeGame()
+        {
+            this.createAndPlacePlayer();
+        }
+
+        private void createAndPlacePlayer()
+        {
+            this.player = new Player();
+            this.canvas.Children.Add(this.player.Sprite);
+
+            this.placePlayerNearBottomOfBackgroundCentered();
+        }
+
+        private void placePlayerNearBottomOfBackgroundCentered()
+        {
+            this.player.X = this.canvasWidth / 2 - this.player.Width / 2.0;
+            this.player.Y = this.canvasHeight - this.player.Height - PlayerOffsetFromBottom;
+        }
+
+        /// <summary>
+        /// Moves the player left.
+        /// </summary>
+        public void MovePlayerLeft()
+        {
+            this.player.MoveLeft();
+        }
+
+        /// <summary>
+        /// Moves the player right.
+        /// </summary>
+        public void MovePlayerRight()
+        {
+            this.player.MoveRight();
+        }
+
+        #endregion
+
+    }
+}
