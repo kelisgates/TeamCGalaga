9d37cd1c8bab239e84690014ea45b17b7950531b
Kelis Gates <kg00281@my.westga.edu>
2024-11-19 14:21:29 -0500

    commit changes

--- Galaga/Model/AttackEnemy.cs
+++ Galaga/Model/AttackEnemy.cs
@@ -76,7 +76,8 @@ namespace Galaga.Model
                 };
 
                 this.canvas.Children.Add(enemyBullet.Sprite);
-                this.collisionManager.StartEnemyBulletMovement(enemyBullet, this.canvas);
+                this.IsShooting = true;
+                //this.startMovement(enemyBullet);
             }
             catch (Exception ex)
             {
@@ -84,6 +85,146 @@ namespace Galaga.Model
                 Debug.WriteLine(ex.StackTrace);
             }
         }
+
+        //private void startMovement(Bullet bulletParam)
+        //{
+        //    try
+        //    {
+        //        var seconds = 50;
+
+        //        var timer = new DispatcherTimer
+        //        {
+        //            Interval = TimeSpan.FromMilliseconds(seconds)
+        //        };
+
+        //        timer.Tick += (s, e) =>
+        //        {
+        //            try
+        //            {
+        //                var position = bulletParam.Y;
+        //                var canvasHeight = this.canvas.ActualHeight;
+        //                var canvasBarrier = Math.Min(600, canvasHeight);
+
+        //                Debug.WriteLine($"Bullet Position: {position}, Canvas Barrier: {canvasBarrier}");
+
+        //                if (position < canvasBarrier)
+        //                {
+        //                    var movementPerStep = 10;
+        //                    bulletParam.Y += movementPerStep;
+        //                    this.checkCollision(bulletParam);
+        //                }
+        //                else
+        //                {
+        //                    this.canvas.Children.Remove(bulletParam.Sprite);
+        //                    timer.Stop();
+        //                    this.IsShooting = false;
+        //                    Debug.WriteLine("Bullet removed from canvas.");
+        //                }
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                Debug.WriteLine($"Exception in timer.Tick: {ex.Message}");
+        //                Debug.WriteLine(ex.StackTrace);
+        //            }
+        //        };
+
+        //        timer.Start();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Debug.WriteLine($"Exception in startMovement: {ex.Message}");
+        //        Debug.WriteLine(ex.StackTrace);
+        //    }
+        //}
+
+        //private void checkCollision(Bullet enemyBullet)
+        //{
+        //    try
+        //    {
+        //        if (this.isCollisionProcessed)
+        //        {
+        //            return;
+        //        }
+
+        //        if (enemyBullet.IntersectsWith(this.player))
+        //        {
+        //            this.updateGameState(enemyBullet);
+        //            this.checkPlayerStatus();
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Debug.WriteLine($"Exception in checkCollision: {ex.Message}");
+        //        Debug.WriteLine(ex.StackTrace);
+        //    }
+        //}
+
+        //private void updateGameState(Bullet enemyBullet)
+        //{
+        //    this.isCollisionProcessed = true;
+
+        //    this.gameManager.Player.Lives--;
+        //    this.gameManager.OnPlayerHit(); 
+
+        //    this.canvas.Children.Remove(enemyBullet.Sprite);
+
+        //    this.Timer.Stop();
+        //    this.IsShooting = false;
+        //}
+        ///// <summary>
+        ///// move to player
+        ///// </summary>
+        //private void checkPlayerStatus()
+        //{
+        //    try
+        //    {
+        //        if (this.gameManager.Player.Lives == 0)
+        //        {
+        //            this.Timer.Stop();
+        //            this.gameManager.OnGameOver();
+        //        }
+        //        else
+        //        {
+        //            this.canvas.Children.Remove(this.player.Sprite);
+        //            this.player.StartInvincibility(2);
+
+        //            var playerReturnTimer = new DispatcherTimer
+        //            {
+        //                Interval = TimeSpan.FromSeconds(1)
+        //            };
+        //            this.playerReturnTimer(playerReturnTimer);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Debug.WriteLine($"Exception in checkPlayerStatus: {ex.Message}");
+        //        Debug.WriteLine(ex.StackTrace);
+        //    }
+        //}
+        ///// <summary>
+        ///// move to player
+        ///// </summary>
+        ///// <param name="playerReturnTimer"></param>
+        //private void playerReturnTimer(DispatcherTimer playerReturnTimer)
+        //{
+
+        //    playerReturnTimer.Tick += (s, e) =>
+        //    {
+        //        try
+        //        {
+        //            this.canvas.Children.Add(this.player.Sprite);
+        //            playerReturnTimer.Stop();
+        //            this.isCollisionProcessed = false;
+        //            this.restartShootingTimer();
+        //        }
+        //        catch (Exception ex)
+        //        {
+        //            Debug.WriteLine($"Exception in playerReturnTimer: {ex.Message}");
+        //            Debug.WriteLine(ex.StackTrace);
+        //        }
+        //    };
+        //    playerReturnTimer.Start();
+        //}
 
         private void restartShootingTimer()
         {
